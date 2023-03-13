41f1152ac41540daf5c4afe517a541317a3cf57a
bharathrames <124970053+bharathrames@users.noreply.github.com>
2023-03-13 12:25:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+    var res1=document.getElementById("first").value;
+    console.log(res1)
+    var res2=document.getElementById("middle").value;
+    console.log(res2)
+    var res3=document.getElementById("last").value;
+    console.log(res3)
+    var res4=document.getElementById("mail").value;
+    console.log(res4)
+    var res5=document.getElementById("pass").value;
+    console.log(res5)
+}
